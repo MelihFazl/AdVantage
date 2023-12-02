f39d951cc977605fb2dd86886af0578fe373d129
Utku Kurtulmuş <utkukurtulmus@hotmail.com>
2023-12-02 15:59:59 +0300

    Deneme

--- backend/advantage/src/main/java/com/advantage/advantage/AdvantageApplication.java
+++ backend/advantage/src/main/java/com/advantage/advantage/AdvantageApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AdvantageApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Canın sagolsun");
 		SpringApplication.run(AdvantageApplication.class, args);
 	}
 
