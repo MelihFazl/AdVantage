896819885f0f2d4befb316193795f6a90ce07f52
Melih Fazıl Keskin <melihfazlkeskin@gmail.com>
2023-12-02 15:21:23 +0300

    Spring Boot initial config

---
+++ backend/advantage/src/main/java/com/advantage/advantage/AdvantageApplication.java
@@ -0,0 +1,13 @@
+package com.advantage.advantage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvantageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvantageApplication.class, args);
+	}
+
+}
---
+++ backend/advantage/src/test/java/com/advantage/advantage/AdvantageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advantage.advantage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvantageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
