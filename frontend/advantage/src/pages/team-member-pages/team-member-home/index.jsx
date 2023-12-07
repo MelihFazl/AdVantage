ce2ddf39cbdc93f20f23dc0ffdac7387189738b4
Melih Fazıl Keskin <melihfazlkeskin@gmail.com>
2023-12-07 23:19:36 +0300

    Report Listing field + mock items

--- frontend/advantage/src/pages/team-member-pages/team-member-home/index.jsx
+++ frontend/advantage/src/pages/team-member-pages/team-member-home/index.jsx
@@ -1,8 +1,9 @@
 import LeftDrawer from "../../../common/left-drawer";
 import React from "react";
 import { TeamMemberDrawerItems } from "../team-member-drawer-items";
-import { Box, Stack, Typography } from "@mui/material";
+import { Paper, Box, Stack, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import ReportListCard from "./report-list-card";
 
 const BannerText = styled(Typography)({
   textAlign: "center",
@@ -17,32 +18,78 @@ const TeamText = styled(Typography)({
   fontSize: "32px",
 });
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: "#000",
+}));
+
 export const TeamMemberHomePage = () => {
   return (
     <Stack direction={"row"}>
       <LeftDrawer drawerItems={TeamMemberDrawerItems}></LeftDrawer>
-      <Stack direction="column" width="100%">
+      <Stack
+        direction="column"
+        width="100%"
+        position={"relative"}
+        margin={0}
+        padding={0}
+        backgroundColor={"#FFFFFF"}
+      >
         <Box
+          minWidth={"530px"}
           display={"flex"}
           flexGrow={"1"}
           flexShrink={"1"}
           flexBasis={"auto"}
           alignItems="center"
           justifyContent="center"
-          marginTop={"20px"}
-          padding={0}
+          position={"sticky"}
+          paddingTop={"20px"}
+          top={0}
+          backgroundColor={"#FFFFFF"}
         >
           <BannerText>Advantage Advertisement Suggestion Tool</BannerText>
         </Box>
         <Box
+          minWidth={"530px"}
           display={"flex"}
           flexGrow={"1"}
           flexShrink={"1"}
           flexBasis={"auto"}
           justifyContent="center"
           alignItems="center"
+          position={"sticky"}
+          backgroundColor={"#FFFFFF"}
+          paddingBottom={"32px"}
+          top={50}
         >
           <TeamText>See team Veni Vidi Code's previous reports</TeamText>
+        </Box>
+        <Box sx={{ width: 1 }}>
+          <Box
+            display="grid"
+            gridTemplateColumns="repeat(auto-fit,  minmax(263.2px, 1fr))"
+            gap={"20px"}
+            padding={"5px 80px 80px 80px"}
+            flexWrap={"wrap"}
+          >
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+            <ReportListCard></ReportListCard>
+          </Box>
         </Box>
       </Stack>
     </Stack>
