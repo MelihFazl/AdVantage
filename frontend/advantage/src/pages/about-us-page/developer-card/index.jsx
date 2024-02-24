08b9b58bc294d7e79367a6a52cb47c4cb5a2ae4c
Melih Fazıl Keskin <melihfazlkeskin@gmail.com>
2024-02-24 16:42:10 +0300

    Home&About page refactorization

---
+++ frontend/advantage/src/pages/about-us-page/developer-card/index.jsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+export default function DeveloperCard({ name, title, image }) {
+  return (
+    <Card
+      sx={{
+        maxWidth: 260,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        flexDirection: "column",
+      }}
+    >
+      <CardMedia
+        sx={{
+          marginTop: "16px",
+          width: 160,
+          height: 160,
+          borderRadius: "50%", // Make the image round
+        }}
+        image={image}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h6"
+          component="div"
+          color={"#0AD1BF"}
+          fontWeight={"bold"}
+        >
+          {name}
+        </Typography>
+        <Typography variant="body2">{title}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
--- frontend/advantage/src/pages/about-us-page/index.jsx
+++ frontend/advantage/src/pages/about-us-page/index.jsx
@@ -1,9 +1,10 @@
 import { TopBarHome } from "../../common/top-bar-home";
 import React from "react";
 import { styled } from "@mui/material/styles";
-import aboutImage from "../../assets/images/about.png";
+import melihImage from "../../assets/images/melih.jpeg";
+import mirayImage from "../../assets/images/miray.jpeg";
 import { Typography, Card, CardContent, Container } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import DeveloperCard from "./developer-card";
 
 const AboutText = styled(Typography)({
   textAlign: "center",
@@ -33,26 +34,50 @@ const AboutImage = styled("img")({
   borderRadius: "15px",
 });
 
-export const AboutUsPage = () => {
-  const navigate = useNavigate();
+const DeveloperCardsContainer = styled("div")({
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))", // Adjust column width as needed
+  marginTop: "20px",
+  gap: "12px",
+});
 
+export const AboutUsPage = () => {
   return (
     <div
       style={{ backgroundColor: "rgba(242, 244, 248, 1)", minHeight: "100vh" }}
     >
       <TopBarHome />
-      <Container maxWidth="md">
+      <Container padding="0 100px 50px 100px" marginBottom="50px">
         <AboutText variant="h5">ABOUT</AboutText>
         <WhoWeAreText>Who we are? Your trust is what we need!</WhoWeAreText>
 
-        <AboutImageCard>
-          <AboutImage src={aboutImage} alt="About Us" />
-          <CardContent>
-            {/* Additional content can be added here */}
-          </CardContent>
-        </AboutImageCard>
-
-        {/* Additional content can be added below the image */}
+        <DeveloperCardsContainer>
+          <DeveloperCard
+            name="Kerem Şahin"
+            title="Part-Time Researcher @Bilkent University Software Engineering and Data Analytics Research Group"
+            image={melihImage}
+          ></DeveloperCard>
+          <DeveloperCard
+            name="Kutay Şenyiğit"
+            title="Game Developer : Partnered with the biggest company in the industry"
+            image={melihImage}
+          ></DeveloperCard>
+          <DeveloperCard
+            name="Miray Ayerdem"
+            title="Part-Time Researcher @Bilkent University Software Engineering and Data Analytics Research Group"
+            image={mirayImage}
+          ></DeveloperCard>
+          <DeveloperCard
+            name="Utku Kurtulmuş"
+            title="Full-Time Bilkent CS Senior Student"
+            image={melihImage}
+          ></DeveloperCard>
+          <DeveloperCard
+            name="Melih Fazıl Keskin"
+            title="Part-time Software Engineer @ Actioner"
+            image={melihImage}
+          ></DeveloperCard>
+        </DeveloperCardsContainer>
       </Container>
     </div>
   );
